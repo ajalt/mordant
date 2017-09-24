77369bbd0e82f40ab74ed95a39f48080dd9d6103
AJ Alt <aj@alt>
2017-09-24 16:14:28 -0700

    Use HSV color square in demo.

--- samples/src/main/kotlin/com/github/ajalt/termcolors/samples/main.kt
+++ samples/src/main/kotlin/com/github/ajalt/termcolors/samples/main.kt
@@ -5,9 +5,15 @@ import com.github.ajalt.termcolors.Ansi256ColorCode
 import com.github.ajalt.termcolors.TermColors
 
 fun demo() = TermColors(TermColors.Level.TRUECOLOR).run {
-    val title = (white + bold)
-    println(title("16 color mode\n"))
-    val colors16 = ((30..37).toList() + (90..97)).map { Ansi16ColorCode(it) }
+    val title = (bold + underline)
+
+    println(title("text styles\n"))
+    println("${bold("bold")} ${dim("dim")} ${italic("italic")} ${underline("underline")} " +
+            "${inverse("inverse")} ${strikethrough("strikethrough")}")
+
+
+    println(title("\n\n16 color mode\n"))
+    val colors16 = ((30..37) + (90..97)).map { Ansi16ColorCode(it) }
     for (fg in colors16) {
         for (bg in colors16) {
             print("${(fg on bg)(" ::: ")} ")
@@ -15,9 +21,9 @@ fun demo() = TermColors(TermColors.Level.TRUECOLOR).run {
         println()
     }
 
-    println(title("\n\n256 color mode\n"))
 
-    for (row in (16..46 step 6)+(124..154 step 6)) {
+    println(title("\n\n256 color mode\n"))
+    for (row in (16..46 step 6) + (124..154 step 6)) {
         for (space in arrayOf(true, false, true)) {
             for (block in (row..(row + 72) step 36)) {
                 for (cell in (block..(block + 5))) {
@@ -32,31 +38,26 @@ fun demo() = TermColors(TermColors.Level.TRUECOLOR).run {
     }
     println()
 
+
     println(title("\n\n256 color mode grayscale\n"))
     for (i in 232..255) {
         print(Ansi256ColorCode(i).bg("   "))
     }
     println()
 
-    println(title("\n\n${red("R")}${green("G")}${blue("B")} true color 24-bit mode\n"))
 
-    for (ri in (0..255 step 128)) {
-        for (g in 0..255 step (255 / 18)) {
-            for (r in ri..(ri + 128) step (255 / 6)) {
-                print("  ")
-                for (b in 0..255 step (255 / 31)) {
-                    print(rgb(r, g, b).bg(" "))
-                }
-            }
-            println()
+    println(title("\n\n${red("R")}${green("G")}${blue("B")} true color 24-bit mode\n"))
+    for (v in 0..100 step 3) {
+        for (h in 0..360 step 3) {
+            print(hsv(h, 100, 100 - v).bg(" "))
         }
         println()
     }
 
-    println(title("\n\ntrue color 24-bit mode grayscale\n"))
 
-    for (i in 1..136) {
-        print(gray(i / 136.0).bg(" "))
+    println(title("\n\ntrue color 24-bit mode grayscale\n"))
+    for (i in 1..120) {
+        print(gray(i / 120.0).bg(" "))
     }
     println()
 }
