e0472ef86b3be9e12baeee823c62118fed16bca4
AJ <aj@alt>
2020-10-23 09:06:53 -0700

    Detect even more terminals

--- mordant/src/jvmMain/kotlin/com/github/ajalt/mordant/terminal/TerminalDetection.kt
+++ mordant/src/jvmMain/kotlin/com/github/ajalt/mordant/terminal/TerminalDetection.kt
@@ -23,20 +23,31 @@ internal object TerminalDetection {
 
     // https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
     private fun ansiHyperLinks(): Boolean {
-        return isWindowsTerminal() || when (getTermProgram()) {
+        return forcedColor() != NONE && (isWindowsTerminal() || when (getTermProgram()) {
             "hyper", "wezterm" -> true
             "iterm.app" -> isRecentITerm()
             else -> false
-        }
+        })
     }
 
+
     private fun ansiLevel(): AnsiLevel {
+        forcedColor()?.let { return it }
+
+        // Terminals embedded in some IDEs support color even though stdout isn't interactive. Check
+        // those terminals before checking stdout.
         if (isIntellijConsole()) return ANSI16
         if (isVsCodeTerminal()) return ANSI256
+
+        // If stdout isn't interactive, never output colors, since we might be redirected to a file etc.
+        if (!stdoutInteractive()) return NONE
+
+        // Otherwise check the large variety of environment variables set by various terminal
+        // emulators to detect color support
+
         if (isWindowsTerminal() || isDomTerm()) return TRUECOLOR
 
-        // https://github.com/termstandard/colors/
-        when (System.getenv("COLORTERM")?.toLowerCase()) {
+        when (getColorTerm()) {
             "24bit", "24bits", "truecolor" -> return TRUECOLOR
         }
 
@@ -47,12 +58,17 @@ internal object TerminalDetection {
             "wezterm" -> return TRUECOLOR
         }
 
-        val term = System.getenv("TERM")?.toLowerCase()
-        when (term?.split("-")?.takeIf { it.size > 1 }?.last()) {
+        val (term, level) = System.getenv("TERM")?.toLowerCase()?.split("-")
+                ?.let { it.firstOrNull() to it.lastOrNull() }
+                ?: null to null
+
+        when (level) {
             "256", "256color", "256colors" -> return ANSI256
             "24bit", "24bits", "direct", "truecolor" -> return TRUECOLOR
         }
 
+        // If there's no explicit level (like "xterm") or the level is ansi16 (like "rxvt-16color"),
+        // just look at the terminal value
         return when (term) {
             "cygwin" -> when {
                 // New versions of windows 10 cmd.exe supports truecolor, and most other terminal emulators
@@ -60,9 +76,25 @@ internal object TerminalDetection {
                 System.getProperty("os.name") == "Windows 10" -> TRUECOLOR
                 else -> ANSI256
             }
-            "xterm", "vt100", "vt220", "screen", "ansi", "rxvt" -> ANSI16
+            "xterm", "vt100", "vt220", "screen", "ansi", "rxvt", "konsole" -> ANSI16
             "dumb" -> NONE
             else -> NONE
+        }
+    }
+
+    // https://github.com/termstandard/colors/
+    private fun getColorTerm() = System.getenv("COLORTERM")?.toLowerCase()
+
+    private fun forcedColor(): AnsiLevel? {
+        return when {
+            System.getenv("NO_COLOR") != null -> NONE // https://no-color.org/
+            else -> when (System.getenv("FORCE_COLOR")) {
+                "0", "false", "none" -> NONE
+                "1", "", "true", "16color" -> ANSI16
+                "2", "256color" -> ANSI256
+                "3", "truecolor" -> TRUECOLOR
+                else -> null
+            }
         }
     }
 
