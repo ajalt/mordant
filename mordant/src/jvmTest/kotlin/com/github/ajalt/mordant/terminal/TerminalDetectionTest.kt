7bce79e5e380a486fc04f528ee4fea553eea780e
AJ <aj@alt>
2022-07-30 10:56:04 -0700

    Print output

--- mordant/src/jvmTest/kotlin/com/github/ajalt/mordant/terminal/TerminalDetectionTest.kt
+++ mordant/src/jvmTest/kotlin/com/github/ajalt/mordant/terminal/TerminalDetectionTest.kt
@@ -1,12 +1,28 @@
 package com.github.ajalt.mordant.terminal
 
 import com.github.ajalt.mordant.internal.getTerminalSize
+import com.github.ajalt.mordant.internal.isWindows
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import java.util.concurrent.TimeUnit
 import kotlin.test.Test
 
 class TerminalDetectionTest {
     @Test
     fun testDetectWidth() {
-        getTerminalSize(1000, true).shouldNotBeNull()
+        if( isWindows()) return
+        val cmd = ProcessBuilder("/usr/bin/env", "stty", "size")
+        val proc =    cmd.redirectInput(ProcessBuilder.Redirect.INHERIT)
+            .start()
+        proc.waitFor(5, TimeUnit.SECONDS)
+        val e = proc.errorStream.bufferedReader().readText()
+        val o = proc.inputStream.bufferedReader().readText()
+        """
+        ---e
+            $e
+        ---
+            $o
+        ---
+        """ shouldBe ""
     }
 }
