81013b632dc06401413c5deaf3fb115c83efef2b
AJ <aj@alt>
2023-06-12 10:14:04 -0700

    Split terminal constructor to avoid OptIn warning

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -3,8 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 subprojects {
     tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
-            jvmTarget = "1.8"
-            freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+            freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
         }
     }
 }
