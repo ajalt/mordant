44de090ca3f00d0d30832a2b873d461fadad37ae
Sebastian Schuberth <sschuberth@users.noreply.github.com>
2023-09-18 19:07:52 +0200

    Add a detection sample (#126)
    * Turn `TerminalInfo` into a `data class`
    
    This allows to easily compare instances and benefit from an
    auto-generated `toString()` representation.
    
    The downside is that `width` and `height` could now be set from the
    outside after construction, but that is probably not too much of an
    issue as arbitrary values could also have been passed via the
    constructor initially anyway.
    
    * Add a basic sample to show information for issue reports
    
    Later on printing of selected environment variables or similar useful
    output could be added.

---
+++ samples/detection/README.md
@@ -0,0 +1,3 @@
+# Detection Sample
+
+This sample shows the detected terminal, e.g. to collect information for issue reports.
---
+++ samples/detection/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    kotlin("multiplatform")
+    application
+}
+
+kotlin {
+    jvm {
+        withJava()
+    }
+
+    macosX64()
+    linuxX64()
+    mingwX64()
+
+    targets.filterIsInstance<KotlinNativeTarget>().forEach { target ->
+        target.binaries.executable {
+            entryPoint = "com.github.ajalt.mordant.samples.main"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":mordant"))
+            }
+        }
+    }
+}
+
+application {
+    mainClass.set("com.github.ajalt.mordant.samples.MainKt")
+    applicationDefaultJvmArgs = listOf("-Dfile.encoding=utf-8")
+}
