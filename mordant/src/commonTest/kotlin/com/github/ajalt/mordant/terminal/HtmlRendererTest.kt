bec3fcaa696acbddd20a0eff89f6eb738343743a
AJ <aj@alt>
2024-01-12 11:10:23 -0800

    Fix HtmlRendererTest

--- mordant/src/commonTest/kotlin/com/github/ajalt/mordant/terminal/HtmlRendererTest.kt
+++ mordant/src/commonTest/kotlin/com/github/ajalt/mordant/terminal/HtmlRendererTest.kt
@@ -33,7 +33,7 @@ class HtmlRendererTest {
     @JsName("frame_no_body_tag")
     fun `frame no body tag`() {
         vt.outputAsHtml(includeCodeTag = false, includeBodyTag = false) shouldBe """
-        |<div style="border-radius: 8px;width: fit-content;padding: 0.5em 1em;filter: drop-shadow(0.5em 0.5em 0.5em black);background-color: rgb(12 12 12);"><span style="color: #ff5f56;">⏺&nbsp;</span><span style="color: #ffbd2e;">⏺&nbsp;</span><span style="color: #27c93f;">⏺&nbsp;</span>
+        |<div style="border-radius: 8px;width: fit-content;padding: 0.5em 1em;filter: drop-shadow(0.5em 0.5em 0.5em black);background-color: rgb(12 12 12);">\n<div style="margin: -0.75em 0px;font-size: 2em"><span style="color: #ff5f56;">⏺&nbsp;</span><span style="color: #ffbd2e;">⏺&nbsp;</span><span style="color: #27c93f;">⏺&nbsp;</span></div>
         |<pre style="font-family: monospace">
         |<span style="color: #800000">red red</span><span>plain</span>
         |<span style="color: #000080">blue blue</span>
