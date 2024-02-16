53c25d88ee3b286e71293d63808ef09f23435602
AJ <aj@alt>
2024-02-16 10:49:24 -0800

    Enable dokka multi-module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,24 @@
+import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     alias(libs.plugins.kotlinBinaryCompatibilityValidator)
+    id("org.jetbrains.dokka")
 }
 
 apiValidation {
     // https://github.com/Kotlin/binary-compatibility-validator/issues/3
     project("samples").subprojects.mapTo(ignoredProjects) { it.name }
     project("test").subprojects.mapTo(ignoredProjects) { it.name }
+}
+
+tasks.withType<DokkaMultiModuleTask>().configureEach {
+    outputDirectory.set(rootProject.rootDir.resolve("docs/api"))
+    pluginsMapConfiguration.set(
+        mapOf(
+            "org.jetbrains.dokka.base.DokkaBase" to """{
+                "footerMessage": "Copyright &copy; 2017 AJ Alt"
+            }"""
+        )
+    )
 }
--- deploy_website.sh
+++ deploy_website.sh
@@ -9,7 +9,7 @@
 set -ex
 
 # Generate API docs
-./gradlew dokkaHtml
+./gradlew dokkaHtmlMultiModule
 
 # Copy the changelog into the site, omitting the unreleased section
 cat CHANGELOG.md \
