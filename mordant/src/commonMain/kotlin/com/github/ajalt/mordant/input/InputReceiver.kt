321a42441d81d07553f996c89ebef7ad9bbae4d9
AJ <aj@alt>
2024-06-10 15:59:38 -0700

    Add Terminal.receiveEvents

--- mordant/src/commonMain/kotlin/com/github/ajalt/mordant/input/InputReceiver.kt
+++ mordant/src/commonMain/kotlin/com/github/ajalt/mordant/input/InputReceiver.kt
@@ -3,6 +3,9 @@ package com.github.ajalt.mordant.input
 // TODO: docs
 interface InputReceiver<T> {
     sealed class Status<out T> {
+        companion object {
+            val Finished = Finished(Unit)
+        }
         data object Continue : Status<Nothing>()
         data class Finished<T>(val result: T) : Status<T>()
     }
