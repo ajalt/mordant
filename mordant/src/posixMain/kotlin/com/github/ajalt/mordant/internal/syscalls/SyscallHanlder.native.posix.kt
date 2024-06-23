e81853f1a2f0440f1f9dd12cd3e694f4553d6c64
AJ <aj@alt>
2024-06-23 08:51:43 -0700

    Spit linux and apple native implementations

--- mordant/src/posixMain/kotlin/com/github/ajalt/mordant/internal/syscalls/SyscallHanlder.native.posix.kt
+++ mordant/src/posixMain/kotlin/com/github/ajalt/mordant/internal/syscalls/SyscallHanlder.native.posix.kt
@@ -1,55 +1,26 @@
 package com.github.ajalt.mordant.internal.syscalls
 
 import com.github.ajalt.mordant.internal.Size
+import com.github.ajalt.mordant.internal.syscalls.SyscallHandlerPosix.Termios
 import kotlinx.cinterop.*
-import platform.posix.*
+import platform.posix.read
 import kotlin.time.ComparableTimeMark
 import kotlin.time.Duration
+
+internal expect fun getTerminalSizeNative(): Size?
+internal expect fun getStdinTermiosNative(): Termios
+internal expect fun setStdinTermiosNative(termios: Termios)
 
 internal object SyscallHandlerNativePosix : SyscallHandlerPosix() {
     override fun isatty(fd: Int): Boolean {
         return platform.posix.isatty(fd) != 0
     }
 
-    override fun getTerminalSize(): Size? = memScoped {
-        val size = alloc<winsize>()
-        if (ioctl(platform.posix.STDIN_FILENO, TIOCGWINSZ.toULong(), size) < 0) {
-            null
-        } else {
-            Size(width = size.ws_col.toInt(), height = size.ws_row.toInt())
-        }
-    }
+    override fun getTerminalSize(): Size? = getTerminalSizeNative()
 
-    override fun getStdinTermios(): Termios = memScoped {
-        val termios = alloc<termios>()
-        if (tcgetattr(STDIN_FILENO, termios.ptr) != 0) {
-            throw RuntimeException("Error reading terminal attributes")
-        }
-        return Termios(
-            iflag = termios.c_iflag.convert(),
-            oflag = termios.c_oflag.convert(),
-            cflag = termios.c_cflag.convert(),
-            lflag = termios.c_lflag.convert(),
-            cc = ByteArray(NCCS) { termios.c_cc[it].convert() },
-        )
-    }
+    override fun getStdinTermios(): Termios = getStdinTermiosNative()
 
-    override fun setStdinTermios(termios: Termios): Unit = memScoped {
-        val nativeTermios = alloc<termios>()
-        // different platforms have different fields in termios, so we need to read the current
-        // struct before we set the fields we carre about.
-        if (tcgetattr(STDIN_FILENO, nativeTermios.ptr) != 0) {
-            throw RuntimeException("Error reading terminal attributes")
-        }
-        nativeTermios.c_iflag = termios.iflag.convert()
-        nativeTermios.c_oflag = termios.oflag.convert()
-        nativeTermios.c_cflag = termios.cflag.convert()
-        nativeTermios.c_lflag = termios.lflag.convert()
-        repeat(NCCS) { nativeTermios.c_cc[it] = termios.cc[it].convert() }
-        if (tcsetattr(platform.posix.STDIN_FILENO, TCSADRAIN, nativeTermios.ptr) != 0) {
-            throw RuntimeException("Error setting terminal attributes")
-        }
-    }
+    override fun setStdinTermios(termios: Termios): Unit = setStdinTermiosNative(termios)
 
     override fun readRawByte(t0: ComparableTimeMark, timeout: Duration): Char = memScoped {
         do {
