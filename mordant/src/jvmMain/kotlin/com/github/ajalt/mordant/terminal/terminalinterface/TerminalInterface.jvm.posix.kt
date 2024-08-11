d3ccd060b793a8e0593e92018aff3de7d9748b40
AJ <aj@alt>
2024-08-11 10:03:15 -0700

    Dump API

--- mordant/src/jvmMain/kotlin/com/github/ajalt/mordant/terminal/terminalinterface/TerminalInterface.jvm.posix.kt
+++ mordant/src/jvmMain/kotlin/com/github/ajalt/mordant/terminal/terminalinterface/TerminalInterface.jvm.posix.kt
@@ -3,7 +3,9 @@ package com.github.ajalt.mordant.terminal.terminalinterface
 import kotlin.time.ComparableTimeMark
 import kotlin.time.Duration
 
-// TODO: docs
+/**
+ * A base class for terminal interfaces for JVM POSIX systems that uses `System.in` for input.
+ */
 abstract class TerminalInterfaceJvmPosix : TerminalInterfacePosix() {
     override fun readRawByte(t0: ComparableTimeMark, timeout: Duration): Char {
         do {
