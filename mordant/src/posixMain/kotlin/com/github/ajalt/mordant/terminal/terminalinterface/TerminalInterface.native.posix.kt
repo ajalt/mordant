fee97fef7955c789e4436ceffabd3f594ade578d
AJ <aj@alt>
2024-08-25 09:56:52 -0700

    Extract native `read` calls

--- mordant/src/posixMain/kotlin/com/github/ajalt/mordant/terminal/terminalinterface/TerminalInterface.native.posix.kt
+++ mordant/src/posixMain/kotlin/com/github/ajalt/mordant/terminal/terminalinterface/TerminalInterface.native.posix.kt
@@ -1,7 +1,9 @@
 package com.github.ajalt.mordant.terminal.terminalinterface
 
-import kotlinx.cinterop.*
-import platform.posix.read
+import kotlinx.cinterop.ByteVar
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.value
 import kotlin.time.ComparableTimeMark
 import kotlin.time.Duration
 
@@ -13,10 +15,13 @@ internal abstract class TerminalInterfaceNativePosix : TerminalInterfacePosix() 
     override fun readRawByte(t0: ComparableTimeMark, timeout: Duration): Char = memScoped {
         do {
             val c = alloc<ByteVar>()
-            val read = read(platform.posix.STDIN_FILENO, c.ptr, 1u)
+            val read = readIntoBuffer(c)
             if (read < 0) throw RuntimeException("Error reading from stdin")
             if (read > 0) return c.value.toInt().toChar()
         } while (t0.elapsedNow() < timeout)
         throw RuntimeException("Timeout reading from stdin (timeout=$timeout)")
     }
+
+    // `read` has different byte widths on linux and apple
+    protected abstract fun readIntoBuffer(c: ByteVar): Long
 }
