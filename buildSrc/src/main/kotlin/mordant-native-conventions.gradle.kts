2e3d31370d814f464515d1a2a23f3b94b9b1c6a2
AJ <aj@alt>
2024-09-02 15:09:28 -0700

    Use gradle instead of manual copy-pasting

--- buildSrc/src/main/kotlin/mordant-native-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/mordant-native-conventions.gradle.kts
@@ -8,6 +8,17 @@ kotlin {
     tvosX64()
     tvosArm64()
     tvosSimulatorArm64()
+    watchosArm32()
     watchosArm64()
     watchosX64()
+    watchosSimulatorArm64()
+
+    sourceSets {
+        for (target in listOf(
+            "tvosX64", "tvosArm64", "tvosSimulatorArm64",
+            "watchosArm32", "watchosArm64", "watchosX64", "watchosSimulatorArm64",
+        )) {
+            sourceSets.getByName(target + "Main").kotlin.srcDirs("src/posixSharedMain/kotlin")
+        }
+    }
 }
